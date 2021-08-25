67a733bf42c915ca98d6b72c47af8294732c155b
lukey-d <68032934+lukey-d@users.noreply.github.com>
2021-08-25 13:42:41 +1200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='BVT Engineering Methods',
+    version='0.1dev',
+    py_modules=[''],
+    package_data={'': ['Autex Frontier Acoustic Fins.csv']},
+    include_package_data=True,
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+   
+)
