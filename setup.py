f6a8797f31764e26eeee5cf9c313511e4759ae6b
Luke Doyle <luke.doyle@bvt.co.nz>
2021-08-26 15:17:02 +1200

    Edit setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='Product_Information',
-    version='0.2dev',
-    py_modules=[''],
-    package_data={'': ['Autex Frontier Acoustic Fins.csv']},
+    name="Product_Information",
+    version="0.2dev",
+    packages=["Product_Information"],
+    package_data={"": ["Autex Frontier Acoustic Fins.csv"]},
     include_package_data=True,
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-   
+    license="Creative Commons Attribution-Noncommercial-Share Alike license",
 )
