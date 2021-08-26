4519741e7fc4633139d9f0cf1304878eeea1d501
Luke Doyle <luke.doyle@bvt.co.nz>
2021-08-26 15:18:00 +1200

    Edit setup.py file

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="Product_Information",
     version="0.2dev",
     packages=["Product_Information"],
+    package_dir=["Product_Informartion"],
     package_data={"": ["Autex Frontier Acoustic Fins.csv"]},
     include_package_data=True,
     license="Creative Commons Attribution-Noncommercial-Share Alike license",
