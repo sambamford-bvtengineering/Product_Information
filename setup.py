053293114e6dd7a4fb08a8e28364743e01e0b755
Luke Doyle <luke.doyle@bvt.co.nz>
2021-08-27 17:07:11 +1200

    Restructure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="Product_Information",
     version="0.3dev",
-    packages=find_packages(include=["Product_Information"]),
+    packages=find_packages(),
     package_data={"": ["*.csv"]},
     include_package_data=True,
     license="Creative Commons Attribution-Noncommercial-Share Alike license",
