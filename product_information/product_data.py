d014050db630159f14bdf8afcdca64f5c8f88279
mattbishop-bvtengineering <89442276+mattbishop-bvtengineering@users.noreply.github.com>
2021-12-15 17:52:23 +1300

    Update product_data.py
    Add tracklok dataframe function

--- product_information/product_data.py
+++ product_information/product_data.py
@@ -9,3 +9,10 @@ def autex_frontier_acoustic_fins():
     path = importlib.resources.open_text(data, "Autex Frontier Acoustic Fins.csv")
 
     return pd.read_csv(path)
+
+def tracklok():
+    """Return a dataframe containing tracklok data"""
+
+    path = importlib.resources.open_text(data, "tracklok.csv")
+
+    return pd.read_csv(path)
